24c51ecbe65183f206897965dcbb65a452ba010e
Cristian Alejandro Gómez <crisoftpc@yahoo.com>
2016-10-07 16:53:22 -0600

    cambio de ruta de logo dacotrans

--- es/envio.php
+++ es/envio.php
@@ -530,7 +530,7 @@ body{background-color:#f6f6f6}.mso h1{}.mso h1{font-family:Georgia,serif !import
       <div class="header" style="Margin: 0 auto;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 173000px);" id="emb-email-header-container">
       <!--[if (mso)|(IE)]><table align="center" class="header" cellpadding="0" cellspacing="0"><tr><td style="width: 600px"><![endif]-->
         <div class="logo emb-logo-margin-box" style="font-size: 26px;line-height: 32px;Margin-top: 6px;Margin-bottom: 20px;color: #c3ced9;font-family: Roboto,Tahoma,sans-serif;Margin-left: 20px;Margin-right: 20px;" align="center">
-          <div class="logo-center" style="font-size:0px !important;line-height:0 !important;" align="center" id="emb-email-header"><img style="height: auto;width: 100%;border: 0;max-width: 139px;" src="https://i1.createsend1.com/ei/d/BB/FA7/1E9/074703/csfinal/logo_editable_dacotrans.png" alt="" width="139"></div>
+          <div class="logo-center" style="font-size:0px !important;line-height:0 !important;" align="center" id="emb-email-header"><img style="height: auto;width: 100%;border: 0;max-width: 139px;" src="http://dacotrans.com.gt/es/img/logo_corr.png" alt="" width="139"></div>
         </div>
       <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
       </div>
